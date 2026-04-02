79f3a0b2f3cfae419c02db51861fd75b8c03bc73
Tiongi <jaysonmcartagena@gmail.com>
2026-04-02 13:00:28 +0800

    Remove unused Blade view files and log additional OpenRouter API errors related to user authentication.

--- storage/framework/views/39aff42ba64af19dbbba66a76cfc02ac.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('admin.farmers-content', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin-dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\jayso\Desktop\dev\web\Mannalon-App\resources\views/admin/farmers/index.blade.php ENDPATH**/ ?>
--- storage/framework/views/f32faa6a914e5032f064042e3e0acc8b.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('admin.announcements-content', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin-dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\jayso\Desktop\dev\web\Mannalon-App\resources\views/admin/announcements/index.blade.php ENDPATH**/ ?>
